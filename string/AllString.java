85cfe04b29521886c20d63d605e9536be65d2b2a
Ogheneogaga Fidelis Agi <aogaga@gmail.com>
2020-08-29 15:04:29 -0500

    switching machines

--- string/AllString.java
+++ string/AllString.java
@@ -1,5 +1,14 @@
-class AllString{
+public class AllString{
 	public static void main(String [] args){
 		
+		waysOfCreatingString();
+	}
+
+	public static void waysOfCreatingString(){
+
+		String s = new String();
+		char chars[] = {'a', 'b', 'c'};
+		String s2 = new String(chars);
+		System.out.println(s2);
 	}
 }
